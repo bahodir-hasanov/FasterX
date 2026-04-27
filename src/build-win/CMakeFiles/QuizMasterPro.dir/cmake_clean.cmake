0c6a2d47d003286c0eff36e1b7a13dcd302e76f5
bahodir-hasanov <hasanovbahodir136@gmail.com>
2026-04-27 10:46:03 +0500

    .exe

---
+++ src/build-win/CMakeFiles/QuizMasterPro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuizMasterPro.dir/main.cpp.obj"
+  "CMakeFiles/QuizMasterPro.dir/main.cpp.obj.d"
+  "QuizMasterPro.exe"
+  "QuizMasterPro.pdb"
+  "libQuizMasterPro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuizMasterPro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build-win/CMakeFiles/QuizMasterPro.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for QuizMasterPro.
+# This may be replaced when dependencies are built.
